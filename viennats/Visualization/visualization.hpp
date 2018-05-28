40c59401e63c8a5a95e952436fdb38f55167e847
PeterPahs <peter.kramuri@gmail.com>
2018-05-28 15:59:44 +0200

    display cast errors

--- viennats/Visualization/visualization.hpp
+++ viennats/Visualization/visualization.hpp
@@ -285,6 +285,18 @@ namespace lvlset{
         for(typename LevelSetType::const_iterator_runs it(ls); !it.is_finished(); it.next()){
             if(it.is_active()){
               if(D>2){
+                if(fabs(it.start_indices(0)) < std::numeric_limits<float>::epsilon()){
+                  std::cout << "Catch" << std::endl;
+                }
+                if(fabs(it.start_indices(1)) < std::numeric_limits<float>::epsilon()){
+                  std::cout << "Catch" << std::endl;
+                }
+                if(fabs(it.start_indices(2)) < std::numeric_limits<float>::epsilon()){
+                  std::cout << "Catch" << std::endl;
+                }
+                if(fabs(it.value2()) < std::numeric_limits<float>::epsilon()){
+                  std::cout << "Catch" << std::endl;
+                }
                 window.addPoint(it.start_indices(0), it.start_indices(1), it.start_indices(2), it.value2(), open_boundary_direction, is_open_boundary_negative);
               }
               else {
